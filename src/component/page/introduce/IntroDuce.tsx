8ccb9d2782fa437fcbc83671e6f00553b63661b8
SEIMIN-BJJ <interkq12@gmail.com>
2023-09-14 22:40:48 +0900

    .

--- src/component/page/introduce/IntroDuce.tsx
+++ src/component/page/introduce/IntroDuce.tsx
@@ -5,7 +5,7 @@ import "./IntroDuce.scss"
 
 const Introduce = [
   {
-    title: "1.main",
+    title: "main",
     route: "/main"
   }
 ]
@@ -18,30 +18,23 @@ const IntroDuce = () => {
       <article>
         <motion.div
           className="MainWrapper"
-          initial={{ y: "30%", opacity: 1 }}
+          initial={{ y: "25%", opacity: 1 }}
           animate={{ paddingTop: "20%" }}
           transition={{
             ease: "linear",
             duration: 1,
-            y: { duration: 1 }
+            x: { duration: 1 }
           }}
           exit={{ y: window.innerHeight, opacity: 0 }}
         >
           {Introduce.map((work, index) => {
             return (
               <>
-                <button className="MainFocus" onClick={() => navigate(work.route)}>{work.title}</button>
-                <button>
-                  <a href="https://seimin-bjj.github.io/Sound-Novel/" className='MainFocus'>2.sound novel</a>
-                </button>
-
-                <button>
-                  <a href="https://seimin-bjj.github.io/Rock-Festival/" className='MainFocus'>3.rock festival</a>
-                </button>
-
-                <button>
-                  <a href="https://seimin-bjj.github.io/Toy-Project/" className='MainFocus'>4.toy project</a>
-                </button>
+                <div id="IntroDuceRoot">
+                  <div className='SectionPicture'></div>
+                  <div className='SectionInfo'></div>
+                </div>
+                <button className="MainFocus" style={{ padding: "2rem 0 0 0" }} onClick={() => navigate(work.route)}>{work.title}</button>
               </>
             );
           })}
